531aede790988f7c03be5f62f03b49f958efeade
xycarto <ian@xycarto.com>
2024-01-13 12:08:08 +1300

    new dir paths

--- src/build-surface.sh
+++ src/build-surface.sh
@@ -4,6 +4,7 @@ source .creds
 
 WORKUNIT=$1
 STATE=$2
+CORES=$( nproc )
 
 git clone https://${TOKEN}@github.com/xycarto/wesm-surfaces.git
 
@@ -14,5 +15,6 @@ make docker-pull
 echo $WORKUNIT 
 echo $STATE
 make download-pc workunit=$WORKUNIT state=$STATE
+find data/point-clouds/${STATE}/${WORKUNIT} -name "*.laz" | xargs -P $CORES -t -I % make bcm pc=% workunit=$WORKUNIT state=$STATE
 
 exit
