da7ad94e9afd9159564861fe5ba4b7f15bfc94f0
xycarto <ian@xycarto.com>
2024-01-13 20:56:59 +1300

    half cores

--- src/build-bcm.sh
+++ src/build-bcm.sh
@@ -4,7 +4,8 @@ source .creds
 
 WORKUNIT=$1
 STATE=$2
-CORES=$( nproc )
+NPROC=$( nproc )
+CORES=$(( $NPROC / 2 ))
 
 git clone https://${TOKEN}@github.com/xycarto/wesm-surfaces.git
 
@@ -13,7 +14,7 @@ cp -r .creds wesm-surfaces/src/
 cd wesm-surfaces/src
 make docker-pull
 make download-pc workunit=$WORKUNIT state=$STATE
-find data/point-clouds/${STATE}/${WORKUNIT} -name "*.laz" | xargs -P $CORES -t -I % make bcm pc=% workunit=$WORKUNIT state=$STATE
+find data/point-clouds/${STATE}/${WORKUNIT} -name "*.laz" | xargs -P 4 -t -I % make bcm pc=% workunit=$WORKUNIT state=$STATE
 
 exit
 
