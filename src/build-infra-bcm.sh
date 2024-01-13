ead931ef238acaba73904b00786f9c9e024d953c
xycarto <ian@xycarto.com>
2024-01-13 16:36:46 +1300

    move scripts around

---
+++ src/build-infra-bcm.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+source .creds
+
+WORKUNIT=$1
+STATE=$2
+
+cp -r terraform terraform-${WORKUNIT}
+
+cd terraform-${WORKUNIT}
+
+export TF_VAR_instance_type="c5.2xlarge"
+export TF_VAR_volume_size="20"
+export TF_VAR_process_file="build-bcm.sh"
+export TF_VAR_WORKUNIT=$WORKUNIT
+export TF_VAR_STATE=$STATE
+
+terraform init 
+
+terraform apply -auto-approve
+
+terraform validate
+
+terraform destroy -auto-approve
+
+cd ../
+
+rm -rf terraform-${WORKUNIT}
