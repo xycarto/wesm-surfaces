8277f29482aabf2684fb7ff3fc831257aa3592f2
xycarto <ian@xycarto.com>
2024-01-16 08:29:27 +1300

    run DSM only

--- src/build-surface.sh
+++ src/build-surface.sh
@@ -14,31 +14,32 @@ cd wesm-surfaces/src
 make docker-pull
 make download-bcm workunit=$WORKUNIT state=$STATE
 
-## Make TINS
-find data/bcm/${STATE}/${WORKUNIT} -name "*.laz" | \
-    xargs -P ${CORES} -t -I % \
-    make tin pc=% workunit=$WORKUNIT state=$STATE
+# # ## Make TINS
+# # find data/bcm/${STATE}/${WORKUNIT} -name "*.laz" | \
+# #     xargs -P ${CORES} -t -I % \
+# #     make tin pc=% workunit=$WORKUNIT state=$STATE
 
-find data/tin/${STATE}/${WORKUNIT} -maxdepth 1 -name "*.tif" | \
-    xargs -P ${CORES} -t -I % \
-    make hillshade tif=% in_dir=tin workunit=$WORKUNIT state=$STATE
+# # find data/tin/${STATE}/${WORKUNIT} -maxdepth 1 -name "*.tif" | \
+# #     xargs -P ${CORES} -t -I % \
+# #     make hillshade tif=% in_dir=tin workunit=$WORKUNIT state=$STATE
 
-make vrt in_dir=tin workunit=$WORKUNIT state=$STATE
+# # make vrt in_dir=tin workunit=$WORKUNIT state=$STATE
 
-make vrt in_dir=tin/hillshade workunit=$WORKUNIT state=$STATE
+# # make vrt in_dir=tin/hillshade workunit=$WORKUNIT state=$STATE
 
-### Make TIN COGS
-find data/tin/${STATE}/${WORKUNIT} -maxdepth 1 -name "*.tif" | \
-    xargs -P ${CORES} -t -I % \
-    make reproject tif=% in_dir=tin workunit=$WORKUNIT state=$STATE
+# # ### Make TIN COGS
+# # find data/tin/${STATE}/${WORKUNIT} -maxdepth 1 -name "*.tif" | \
+# #     xargs -P ${CORES} -t -I % \
+# #     make reproject tif=% in_dir=tin workunit=$WORKUNIT state=$STATE
 
-find data/tin/${STATE}/${WORKUNIT}/hillshade -maxdepth 1 -name "*.tif" | \
-    xargs -P ${CORES} -t -I % \
-    make reproject tif=% in_dir=tin/hillshade workunit=$WORKUNIT state=$STATE
+# # find data/tin/${STATE}/${WORKUNIT}/hillshade -maxdepth 1 -name "*.tif" | \
+# #     xargs -P ${CORES} -t -I % \
+# #     make reproject tif=% in_dir=tin/hillshade workunit=$WORKUNIT state=$STATE
 
-make cog in_dir=tin workunit=$WORKUNIT state=$STATE
+# # make cog in_dir=tin workunit=$WORKUNIT state=$STATE
 
-make cog in_dir=tin/hillshade workunit=$WORKUNIT state=$STATE
+# # make cog in_dir=tin/hillshade workunit=$WORKUNIT state=$STATE
+
 
 ## Make DSM
 find data/bcm/${STATE}/${WORKUNIT} -name "*.laz" | xargs -P ${CORES} -t -I % make dsm pc=% workunit=$WORKUNIT state=$STATE
