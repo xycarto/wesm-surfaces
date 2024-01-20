1836a39bafc4b4506dbc6e6bbbd0c3b532225fec
xycarto <ian@xycarto.com>
2024-01-20 17:44:34 +1300

    where are vars

--- configs/process-config.sh
+++ configs/process-config.sh
@@ -2,7 +2,7 @@
 
 export WORKUNIT="CA_NoCAL_Wildfires_B1_2018"
 export STATE="California"
-export EC2="t2.large"
+export EC2="t2.micro"
 export VOLUME_SIZE=20
 export TYPE="test"
 export LOCATION="remote"
