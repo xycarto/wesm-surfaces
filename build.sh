9f9c2191416d056704b5e40fea1fe36e715aae3d
xycarto <ian@xycarto.com>
2024-01-20 19:05:00 +1300

    env file

--- build.sh
+++ build.sh
@@ -67,8 +67,9 @@ if [[ $LOCATION = "remote" ]]; then
     git clone --branch refactor https://${TOKEN}@github.com/xycarto/wesm-surfaces.git
     cp -r .creds wesm-surfaces
     cd wesm-surfaces
+    export PROCESS=$1
+    set-data-dir
     echo $PROCESS
-    echo $WORKUNIT
     make test-dirs
     # make download-files 
 elif [[ $LOCATION = "local" ]]; then
