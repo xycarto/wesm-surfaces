9c69b819166e05a69acf08de7e7e89291aafd779
xycarto <ian@xycarto.com>
2024-01-31 11:58:07 +1300

    add get file and loop process

--- src/hillshade.py
+++ src/hillshade.py
@@ -11,22 +11,22 @@ from globals import *
 def main():
     s3 = get_creds()    
 
-    out_hs = f"{HS_DIR}/{os.path.basename(IN_FILE)}"  
+    get_s3_file(s3, f"{DIR_PATH}/{BASENAME}", WESM_SURFACE_BUCKET)
+
+    out_hs = f"{HS_DIR}/{BASENAME}"  
     gdal.DEMProcessing(
         out_hs,
-        IN_FILE,
+        f"{DIR_PATH}/{BASENAME}",
         'hillshade',
         computeEdges=True,
         callback=gdal.TermProgress_nocb,
     )
 
-    print(f"Uploading {out_hs}...")
-    s3.upload_file(out_hs, WESM_SURFACE_BUCKET, out_hs)
-
-
 if __name__ == "__main__":
     IN_FILE = sys.argv[1]
-    HS_DIR = f"{DATA_DIR}/{PROCESS}/{STATE}/{WORKUNIT}/hillshade"
+    BASENAME = f"{os.path.basename(IN_FILE).split('.')[0]}.tif"
+    DIR_PATH = os.path.dirname(IN_FILE)
+    HS_DIR = f"{DIR_PATH}/hillshade"
 
     for d in [DATA_DIR, HS_DIR]:
         os.makedirs(d, exist_ok=True)
